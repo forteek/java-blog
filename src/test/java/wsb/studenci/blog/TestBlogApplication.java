9d180799ee0864643d84bd767daf5b5314b9bb2a
Marcin Fortuniak <fortgeek@gmail.com>
2024-11-24 11:56:29 +0100

    init

---
+++ src/test/java/wsb/studenci/blog/TestBlogApplication.java
@@ -0,0 +1,11 @@
+package wsb.studenci.blog;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(BlogApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
